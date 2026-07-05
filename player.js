0f25af3677d4a8913a788f6a0b0c87adb5ad2d0a
chiru-1992 <chirugurung730@gmail.com>
2026-07-05 21:49:56 +0530

    Change player photos to JPG format
    Updated player photos from PNG to JPG format for Egypt and Argentina.

--- player.js
+++ player.js
@@ -1,118 +1,118 @@
 const tickets = [
 
 {
-egypt:{name:"Hamza Abdelkarim",photo:"assets/egypt/hamza_abdelkarim.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"José Manuel López",photo:"assets/argentina/jose_manuel_lopez.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Hamza Abdelkarim",photo:"assets/egypt/hamza_abdelkarim.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"José Manuel López",photo:"assets/argentina/jose_manuel_lopez.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mahmoud Saber",photo:"assets/egypt/mahmoud_saber.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Giuliano Simeone",photo:"assets/argentina/giuliano_simeone.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mahmoud Saber",photo:"assets/egypt/mohmoud_saber.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Giuliano Simeone",photo:"assets/argentina/giuliano_simeone.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mostafa Zico",photo:"assets/egypt/mostafa_zico.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Nico Paz",photo:"assets/argentina/nico_paz.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mostafa Zico",photo:"assets/egypt/mostafa_zico.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Nico Paz",photo:"assets/argentina/nico_paz.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Karim Hafez",photo:"assets/egypt/karim_hafez.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Valentín Barco",photo:"assets/argentina/valentin_barco.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Karim Hafez",photo:"assets/egypt/karim_hafez.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Valentín Barco",photo:"assets/argentina/valentina_barco.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Tarek Alaa",photo:"assets/egypt/tarek_alaa.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Marcos Senesi",photo:"assets/argentina/marcos_senesi.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Tarek Alaa",photo:"assets/egypt/tarek_alaa.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Marcos Senesi",photo:"assets/argentina/marcos_senesi.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mohanad Lashin",photo:"assets/egypt/mohanad_lashin.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Facundo Medina",photo:"assets/argentina/facundo_medina.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mohanad Lashin",photo:"assets/egypt/mohanad_lashin.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Facundo Medina",photo:"assets/argentina/facundo_medina.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Haissem Hassan",photo:"assets/egypt/haissem_hassan.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Gonzalo Montiel",photo:"assets/argentina/gonzalo_montiel.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Haissem Hassan",photo:"assets/egypt/haissem_hassan.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Gonzalo Montiel",photo:"assets/argentina/gonzalo_montiel.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Yasser Ibrahim",photo:"assets/egypt/yasser_ibrahim.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Nicolás Tagliafico",photo:"assets/argentina/tagliafico.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Yasser Ibrahim",photo:"assets/egypt/yasser_ibrahim.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Nicolás Tagliafico",photo:"assets/argentina/nicolas_tagliafico.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Marawan Attia",photo:"assets/egypt/marawan_attia.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Leandro Paredes",photo:"assets/argentina/paredes.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Marawan Attia",photo:"assets/egypt/marawan_attia.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Leandro Paredes",photo:"assets/argentina/leandro_paredes.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Ibrahim Adel",photo:"assets/egypt/ibrahim_adel.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Giovani Lo Celso",photo:"assets/argentina/lo_celso.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Ibrahim Adel",photo:"assets/egypt/ibrahim_adel.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Giovani Lo Celso",photo:"assets/argentina/giovani_lo_celso.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Ahmed Fatouh",photo:"assets/egypt/ahmed_fatouh.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Nahuel Molina",photo:"assets/argentina/nahuel_molina.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Ahmed Fatouh",photo:"assets/egypt/ahmed_fatouh.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Nahuel Molina",photo:"assets/argentina/nahuel_molina.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Hossam Abdelmaguid",photo:"assets/egypt/hossam_abdelmaguid.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Exequiel Palacios",photo:"assets/argentina/palacios.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Hossam Abdelmaguid",photo:"assets/egypt/hossam_abdelmaguid.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Exequiel Palacios",photo:"assets/argentina/exequiel_palacios.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mohamed Hany",photo:"assets/egypt/mohamed_hany.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Nico González",photo:"assets/argentina/nico_gonzalez.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mohamed Hany",photo:"assets/egypt/mohamed_hany.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Nico González",photo:"assets/argentina/nico_gonzalez.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Hamdy Fathy",photo:"assets/egypt/hamdy_fathy.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Rodrigo De Paul",photo:"assets/argentina/de_paul.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Hamdy Fathy",photo:"assets/egypt/hamdy_fathy.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Rodrigo De Paul",photo:"assets/argentina/rodrigo_de_paul.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Nabil Donga",photo:"assets/egypt/nabil_donga.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Enzo Fernández",photo:"assets/argentina/enzo_fernandez.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Nabil Donga",photo:"assets/egypt/nabil_donga.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Enzo Fernández",photo:"assets/argentina/enzo_fernandez.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Rami Rabia",photo:"assets/egypt/rami_rabia.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Nicolás Otamendi",photo:"assets/argentina/otamendi.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Rami Rabia",photo:"assets/egypt/ramy_rabia.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Nicolás Otamendi",photo:"assets/argentina/nicolas_otamendi.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mohamed Abdelmonem",photo:"assets/egypt/mohamed_abdelmonem.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Lisandro Martínez",photo:"assets/argentina/lisandro_martinez.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mohamed Abdelmonem",photo:"assets/egypt/mohamed_abdelmoneim.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Lisandro Martínez",photo:"assets/argentina/lisandro_martinez.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Trezeguet",photo:"assets/egypt/trezeguet.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Thiago Almada",photo:"assets/argentina/thiago_almada.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Trezeguet",photo:"assets/egypt/trezeguet.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Thiago Almada",photo:"assets/argentina/thiayo_almada.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Zizo",photo:"assets/egypt/zizo.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Alexis Mac Allister",photo:"assets/argentina/mac_allister.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Zizo",photo:"assets/egypt/zizo.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Alexis Mac Allister",photo:"assets/argentina/alexis_mac_allister.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Emam Ashour",photo:"assets/egypt/emam_ashour.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Cristian Romero",photo:"assets/argentina/cristian_romero.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Emam Ashour",photo:"assets/egypt/emam_ashour.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Cristian Romero",photo:"assets/argentina/cristian_romero.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Omar Marmoush",photo:"assets/egypt/omar_marmoush.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Julián Álvarez",photo:"assets/argentina/julian_alvarez.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Omar Marmoush",photo:"assets/egypt/omar_marmoush.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Julián Álvarez",photo:"assets/argentina/julian_alvarez.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mohamed Salah",photo:"assets/egypt/mohamed_salah.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Lautaro Martínez",photo:"assets/argentina/lautaro_martinez.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mohamed Salah",photo:"assets/egypt/mohamed_salah.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Lautaro Martínez",photo:"assets/argentina/lautaro_martinez.jpg",jersey:"assets/jerseys/argentina.png"}
 },
 
 {
-egypt:{name:"Mohamed El Shenawy",photo:"assets/egypt/mohamed_el_shenawy.png",jersey:"assets/jerseys/egypt.png"},
-argentina:{name:"Lionel Messi",photo:"assets/argentina/lionel_messi.png",jersey:"assets/jerseys/argentina.png"}
+egypt:{name:"Mohamed El Shenawy",photo:"assets/egypt/mohamed_elshenawy.jpg",jersey:"assets/jerseys/egypt.png"},
+argentina:{name:"Lionel Messi",photo:"assets/argentina/lionel_messi.jpg",jersey:"assets/jerseys/argentina.png"}
 }
 
 ];
